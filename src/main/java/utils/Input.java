3fea6357ddc50aebd8a215447f4d83f0858c91cf
Matt Preston <matt.r.preston@gmail.com>
2023-12-31 00:09:34 +0000

    More refactoring

--- src/main/java/utils/Input.java
+++ src/main/java/utils/Input.java
@@ -7,41 +7,40 @@ import com.google.common.io.CharSource;
 
 public interface Input {
 
-  static Input mockInput(final String text) {
-    return () -> text;
-  }
-
-  static Input input(final Object obj) {
-    final var annotations = obj.getClass().getDeclaredAnnotationsByType(AdventOfCode.class);
-    if (annotations.length == 0) {
-      throw new IllegalArgumentException("@AdventOfCode annotation missing");
-    } else if (annotations.length > 1) {
-      throw new IllegalArgumentException("Too many @AdventOfCode annotations");
+    static Input mockInput(final String text) {
+        return () -> text;
     }
 
-    return input(annotations[0].year(), annotations[0].day());
-  }
+    static Input input(final Object obj) {
+        final var annotations = obj.getClass().getDeclaredAnnotationsByType(AdventOfCode.class);
+        if (annotations.length == 0) {
+            throw new IllegalArgumentException("@AdventOfCode annotation missing");
+        } else if (annotations.length > 1) {
+            throw new IllegalArgumentException("Too many @AdventOfCode annotations");
+        }
 
-  static Input input(final int year, int day) {
-    try {
-      return new CachedInput(year, day);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+        return input(annotations[0].year(), annotations[0].day());
     }
-  }
 
-
-  String text();
-
-  default List<String> lines() {
-    try {
-      return CharSource.wrap(text()).readLines();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    static Input input(final int year, int day) {
+        try {
+            return new RemoteInput(year, day);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  default String[] linesArray() {
-    return lines().toArray(String[]::new);
-  }
+    String text();
+
+    default List<String> lines() {
+        try {
+            return CharSource.wrap(text()).readLines();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    default String[] linesArray() {
+        return lines().toArray(String[]::new);
+    }
 }
---
+++ src/main/java/utils/RemoteInput.java
@@ -0,0 +1,101 @@
+package utils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+
+import static com.google.common.base.Preconditions.checkState;
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.readString;
+import static java.nio.file.Files.writeString;
+
+class RemoteInput implements Input {
+
+    private final Path inputPath;
+
+    RemoteInput(int year, int day) throws Exception {
+        inputPath = Paths.get(format("input/%d/%d-input", year, day));
+
+        if (!inputPath.toFile().exists()) {
+            var parent = inputPath.getParent().toFile();
+            if (!parent.exists()) {
+                checkState(parent.mkdirs());
+            }
+
+            System.out.println("Requesting input file...");
+
+            try (var client = httpClient()) {
+                final var request = HttpRequest.newBuilder()
+                        .uri(URI.create(format("https://adventofcode.com/%d/day/%d/input", year, day)))
+                        .header("Cookie", "session=" + cookieHash())
+                        .timeout(Duration.ofMinutes(1))
+                        .GET()
+                        .build();
+
+                final var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200) {
+                    writeString(inputPath, response.body(), UTF_8);
+                } else {
+                    throw new IllegalStateException("Got an invalid HTTP status reading the input file: " + response.statusCode());
+                }
+            }
+        }
+    }
+
+    @Override
+    public String text() {
+        try {
+            return readString(inputPath, UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String cookieHash() throws IOException {
+        return readString(Paths.get("session"), UTF_8);
+    }
+
+    private HttpClient httpClient() throws NoSuchAlgorithmException, KeyManagementException {
+        return HttpClient.newBuilder()
+                .sslContext(disabledContext())
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+    }
+
+    private SSLContext disabledContext() throws NoSuchAlgorithmException, KeyManagementException {
+        final TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new java.security.cert.X509Certificate[]{};
+                    }
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+        return sslContext;
+    }
+}
