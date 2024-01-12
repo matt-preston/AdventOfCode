844e48d637f7f2a67904c01ab4dcb55f03ec721d
Matt Preston <matt.r.preston@gmail.com>
2024-01-12 09:21:46 +0000

    Generator to create empty solution classes

---
+++ src/main/java/utils/CreateSolutionClass.java
@@ -0,0 +1,55 @@
+package utils;
+
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+
+import static com.google.common.base.Preconditions.checkState;
+
+public class CreateSolutionClass {
+
+    public static void main(String[] args) throws IOException {
+        var year = Integer.parseInt(args[0]);
+        var day = Integer.parseInt(args[1]);
+
+        checkState(year > 2014 && year <= Calendar.getInstance().get(Calendar.YEAR), "[" + year + "] is not a valid year");
+        checkState(day > 0 && day < 26, "[" + day + "] is not a valid day");
+
+        var path = String.format("src/main/java/aoc/y%d/Day%02dSolution.java", year, day);
+        var file = new File(path);
+
+        System.out.println(file.getAbsolutePath());
+
+        if(!file.getParentFile().exists()) {
+            checkState(file.getParentFile().mkdirs());
+        }
+
+        if (file.exists()) {
+            throw new IllegalStateException("Solution class already exists");
+        }
+
+        var template = """
+                package aoc.y%d;
+                
+                import utils.AdventOfCode;
+                import org.junit.jupiter.api.Test;
+                
+                import static org.junit.jupiter.api.Assertions.assertEquals;
+                import static utils.Input.input;
+                import static utils.Input.mockInput;
+                
+                @AdventOfCode(year = %d, day = %d, name = "")
+                public class Day%02dSolution {
+                
+                }
+                """;
+
+        var code = String.format(template, year, year, day, day);
+        System.out.println(code);
+
+        Files.asCharSink(file, StandardCharsets.UTF_8).write(code);
+    }
+}
