d6537195f4c954cc7125da6fe67b88f05a9c515a
Matt Preston <matt.r.preston@gmail.com>
2024-12-29 21:08:43 +0000

    2024 day 21

--- src/main/java/aoc/y2024/Day21Solution.java
+++ src/main/java/aoc/y2024/Day21Solution.java
@@ -1,7 +1,7 @@
 package aoc.y2024;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import com.google.common.collect.*;
+import com.google.common.primitives.Chars;
 import org.junit.jupiter.api.Test;
 import utils.AdventOfCode;
 import utils.Input;
@@ -9,12 +9,13 @@ import utils.Utils;
 import utils.Vector2;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static utils.Input.input;
 import static utils.Input.mockInput;
-import static utils.Utils.contains;
-import static utils.Utils.get;
+import static utils.Utils.*;
 
 @AdventOfCode(year = 2024, day = 21, name = "Keypad Conundrum")
 public class Day21Solution {
@@ -27,167 +28,163 @@ public class Day21Solution {
             379A
             """;
 
-    @Test
-    public void part1WithMockData() {
-        assertEquals(126384, complexity(mockInput(MOCK)));
-    }
-
-
-    // 138560 too high
-    // 135260 too high
-    @Test
-    public void part1() {
-        assertEquals(0, complexity(input(this)));
-    }
-
-    @Test
-    public void part2WithMockData() {
-        assertEquals(0, complexity(mockInput(MOCK)));
-    }
-
-    @Test
-    public void part2() {
-        assertEquals(0, complexity(input(this)));
-    }
-
-    private int complexity(Input input) {
-        return input.lines().stream()
-                .mapToInt(this::complexity)
-                .sum();
-    }
-
-    @Test
-    public void debug() {
-        complexity("456A");
-    }
-
-    private int complexity(String code) {
-        var sequence1 = keypadSequence(NUMERIC_KEYPAD, code);
-        System.out.printf("%s => %s%n", code, sequence1);
-
-        var sequence2 = keypadSequence(DIRECTIONAL_KEYPAD, sequence1);
-        System.out.printf("%s => %s%n", code, sequence2);
-
-        var sequence3 = keypadSequence(DIRECTIONAL_KEYPAD, sequence2);
-        System.out.printf("%s => %s%n", code, sequence3);
-
-        var numericPart = Integer.parseInt(code.replaceAll("\\D", ""));
-
-        System.out.printf("%d * %d%n", sequence3.length(), numericPart);
-        System.out.println();
-
-        return numericPart * sequence3.length();
-    }
-
-
-    private static final char[][] NUMERIC_KEYPAD = Utils.matrix("""
+    private static final Multimap<Path, String> NUMERIC_MAP = pathMap("""
             789
             456
             123
              0A
             """);
 
-    private static final char[][] DIRECTIONAL_KEYPAD = Utils.matrix("""
+    private static final Multimap<Path, String> DIRECTION_MAP = pathMap("""
              ^A
             <v>
             """);
 
+    @Test
+    public void part1WithMockData() {
+        assertEquals(126384, complexity(mockInput(MOCK), 2));
+    }
 
-    private String keypadSequence(char[][] map, String value) {
-        var start = Utils.find(map, 'A');
+    @Test
+    public void part1() {
+        assertEquals(134120, complexity(input(this), 2));
+    }
 
-        var result = "";
+    @Test
+    public void part2WithMockData() {
+        assertEquals(154115708116294L, complexity(mockInput(MOCK), 25));
+    }
 
-        for (char c : value.toCharArray()) {
-            var target = Utils.find(map, c);
-            result += keypadSequence(map, start, target);
-            start = target;
+    @Test
+    public void part2() {
+        assertEquals(167389793580400L, complexity(input(this), 25));
+    }
+
+    private long complexity(Input input, int depth) {
+        var total = 0L;
+        for (String code : input.lines()) {
+            var min = sequences(NUMERIC_MAP, code, 0, 'A', "").stream()
+                    .mapToLong(seq -> shortestSequence(seq, depth))
+                    .min()
+                    .orElseThrow();
+
+            var asNumber = Integer.parseInt(code.replaceAll("\\D", ""));
+            total += asNumber * min;
+        }
+
+        return total;
+    }
+
+    private final Map<String, Long> cache = Maps.newHashMap();
+
+    private long shortestSequence(String keys, int depth) {
+        var cacheKey = keys + depth;
+        if (!cache.containsKey(cacheKey)) {
+            var result = shortestSequenceImpl(keys, depth);
+            cache.put(cacheKey, result);
+            return result;
+        } else {
+            return cache.get(cacheKey);
+        }
+    }
+
+    private long shortestSequenceImpl(String keys, int depth) {
+        if (depth == 0) {
+            return keys.length();
+        } else {
+            var total = 0L;
+
+            var from = 0;
+            var to = 0;
+            while ((to = keys.indexOf('A', from)) > -1) {
+                var subKey = keys.substring(from, to + 1);
+
+                total += sequences(DIRECTION_MAP, subKey, 0, 'A', "").stream()
+                        .mapToLong(seq -> shortestSequence(seq, depth - 1))
+                        .min()
+                        .orElseThrow();
+
+                from = to + 1;
+            }
+
+            return total;
+        }
+    }
+
+    private List<String> sequences(Multimap<Path, String> pathCache, String keys, int index, char previousKey, String currentPath) {
+        if (index == keys.length()) {
+            return List.of(currentPath);
+        }
+
+        var nextKey = keys.charAt(index);
+        var result = Lists.<String>newArrayList();
+
+        for (String path : pathCache.get(new Path(previousKey, nextKey))) {
+            result.addAll(sequences(pathCache, keys, index + 1, nextKey, currentPath + path + 'A'));
         }
 
         return result;
     }
 
-    enum Direction {
-        EAST, WEST, NORTH, SOUTH;
+    record Path(char from, char to) {
+    }
 
-        List<BFSState> next(BFSState state) {
-            var result = Lists.<BFSState>newArrayList();
-            result.add(new BFSState(state.position.add(vector()), this, state.turns())); // same direction
-
-            if (state.turns() < 1) {
-                for (var d : Direction.values()) {
-                    if (d != this) {
-                        result.add(new BFSState(state.position.add(d.vector()), d, state.turns() + 1)); // new direction
-                    }
+    private static Multimap<Path, String> pathMap(String input) {
+        var keypad = Utils.matrix(input);
+        var result = HashMultimap.<Path, String>create();
+        char[] all = Chars.concat(keypad);
+        for (char from : all) {
+            for (char to : all) {
+                if (from != ' ' && to != ' ') {
+                    result.putAll(new Path(from, to), keypadSequence(keypad, from, to));
                 }
             }
-
-            return result;
         }
-
-        Vector2 vector() {
-            return switch (this) {
-                case NORTH -> new Vector2(0, -1);
-                case SOUTH -> new Vector2(0, 1);
-                case EAST -> new Vector2(1, 0);
-                case WEST -> new Vector2(-1, 0);
-            };
-        }
+        return result;
     }
 
-
-    record BFSState(Vector2 position, Direction direction, int turns) {
-        List<BFSState> next() {
-            return direction().next(this);
-        }
-
-        String command() {
-            return switch (direction()) {
-                case NORTH -> "^";
-                case SOUTH -> "v";
-                case EAST -> ">";
-                case WEST -> "<";
-            };
-        }
+    record State(Vector2 position, String path) {
     }
 
-    private String keypadSequence(char[][] map, Vector2 start, Vector2 target) {
-        var frontier = Lists.<BFSState>newLinkedList();
-        frontier.add(new BFSState(start, Direction.EAST, 0));
-        frontier.add(new BFSState(start, Direction.WEST, 0));
-        frontier.add(new BFSState(start, Direction.SOUTH, 0));
-        frontier.add(new BFSState(start, Direction.NORTH, 0));
+    private static Set<String> keypadSequence(char[][] map, char start, char target) {
+        return keypadSequence(map, find(map, start), find(map, target));
+    }
 
-        var parents = Maps.<BFSState, BFSState>newHashMap();
+    private static Set<String> keypadSequence(char[][] map, Vector2 start, Vector2 target) {
+        var frontier = Lists.<State>newLinkedList();
+        frontier.add(new State(start, ""));
+
+        var minimumDistance = Maps.<Vector2, Integer>newHashMap();
+        minimumDistance.put(start, 0);
+
+        var result = Sets.<String>newHashSet();
 
         while (!frontier.isEmpty()) {
             var current = frontier.poll();
 
             if (current.position().equals(target)) {
-                var node = current;
-                var path = "A";
-                while (parents.containsKey(node)) {
-                    var parent = parents.get(node);
-                    path += node.command();
-
-                    if (parent.position().equals(start)) {
-                        break;
-                    }
-
-                    node = parent;
-                }
-
-                return new StringBuilder(path).reverse().toString();
+                result.add(current.path());
             }
 
-            for (BFSState next : current.next()) {
-                if (contains(map, next.position()) && get(map, next.position()) != ' ') {
-                    frontier.add(next);
-                    parents.put(next, current);
+            var next = Map.of(
+                    "^", current.position().north(),
+                    "v", current.position().south(),
+                    ">", current.position().east(),
+                    "<", current.position().west()
+            );
+
+            for (Map.Entry<String, Vector2> entry : next.entrySet()) {
+                var neighbour = entry.getValue();
+                var step = entry.getKey();
+                if (contains(map, neighbour) && get(map, neighbour) != ' ') {
+                    if (minimumDistance.getOrDefault(neighbour, Integer.MAX_VALUE) >= minimumDistance.get(current.position()) + 1) {
+                        minimumDistance.put(neighbour, minimumDistance.get(current.position()) + 1);
+                        frontier.add(new State(neighbour, current.path() + step));
+                    }
                 }
             }
         }
 
-        return "X";
+        return result;
     }
 }
